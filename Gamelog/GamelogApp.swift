29aa42af59c11bc208f7b0d08aa79c95ae980008
kevin.jonathan <kevin.jonathan@gdn-commerce.com>
2022-09-29 14:51:58 +0700

    Initial Commit

---
+++ Gamelog/GamelogApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamelogApp.swift
+//  Gamelog
+//
+//  Created by Kevin Jonathan on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GamelogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
